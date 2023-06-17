412928138dd43b59bc9a7eac012275717e27745f
abrahambeltran <abraham.beltran.2001@gmail.com>
2023-06-17 18:26:42 -0400

    Navbar functional

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,36 @@
-import { useMemo } from "react"
-import { createTheme } from "@mui/material/styles"
-import { themeSettings } from "./theme"
-import { ThemeProvider, CssBaseline } from "@mui/material"
+import { useMemo } from "react";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+import { ThemeProvider, CssBaseline, Box } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "@/scenes/navbar";
+import Dashboard from "@/scenes/dashboard";
 
 function App() {
-  const theme = useMemo(() => createTheme(themeSettings), [])
-  return (
-      <div className='app'>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-        </ThemeProvider>
-      </div>
-  )
+    const theme = useMemo(() => createTheme(themeSettings), []);
+    return (
+        <div className="app">
+            <BrowserRouter>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    <Box
+                        width="100%"
+                        height="100%"
+                        padding="1rem 2rem 4rem 2rem"
+                    >
+                        <Navbar />
+                        <Routes>
+                            <Route path="/" element={<Dashboard />} />
+                            <Route
+                                path="/predictions"
+                                element={<div>predictions page</div>}
+                            />
+                        </Routes>
+                    </Box>
+                </ThemeProvider>
+            </BrowserRouter>
+        </div>
+    );
 }
 
-export default App
+export default App;
