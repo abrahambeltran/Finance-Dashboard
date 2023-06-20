c272b547f188187556e8e24f23e530cb77176364
abrahambeltran <abraham.beltran.2001@gmail.com>
2023-06-20 12:42:27 -0400

    Mongo connection not functional

--- server/index.js
+++ server/index.js
@@ -1,27 +1,34 @@
-import express from "express"
-import mongoose from "mongoose"
-import bodyParser from "body-parser"
-import helmet from "helmet"
-import cors from "cors"
-import dotenv from "dotenv"
-import morgan from "morgan"
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
 
+/* CONFIGURATIONS */
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-const PORT = process.env.PORT || 9000;
-mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(async()=>{
-    app.listen(PORT, () => console.log(`SERVER PORT: ${PORT}`));
-})
-.catch((error) => console.log(`${error} did not connect`));
+/* Mongoose setup */
+const PORT = process.env.PORT || 1337;
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+        /* ADD DATA ONE TIME */
+        // User.insertMany(users);
+        // Post.insertMany(posts);
+    })
+    .catch((error) => console.log(`${error} did not connect`));
