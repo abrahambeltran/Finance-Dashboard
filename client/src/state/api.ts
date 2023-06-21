3a40144313213ffadac2191e9d4c4512dd37e423
abrahambeltran <abraham.beltran.2001@gmail.com>
2023-06-21 18:55:51 -0400

    Update: Row 1 completed

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,5 +1,5 @@
 import {createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-
+import { GetKpisResponse } from "./types";
 export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
     reducerPath: "main",
@@ -8,7 +8,7 @@ export const api = createApi({
     // basically makes a function that gets
     // key performance indicators for the api call
     endpoints: (build) => ({
-        getKpis: build.query<void,void>({
+        getKpis: build.query<Array<GetKpisResponse>,void>({
             query: () => "kpi/kpis/",
             providesTags: ["Kpis"]
         })
