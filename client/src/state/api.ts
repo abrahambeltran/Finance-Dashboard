e32aedcb4a00bdcfa572dae94afecbccbc21a8aa
abrahambeltran <abraham.beltran.2001@gmail.com>
2023-06-23 23:23:15 -0400

    built out api and types for frontend<-backend

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,9 +1,9 @@
 import {createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { GetKpisResponse } from "./types";
+import { GetKpisResponse, GetProductsResponse, GetTransactionsResponse } from "./types";
 export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
     reducerPath: "main",
-    tagTypes: ["Kpis"],
+    tagTypes: ["Kpis", "Products", "Transactions"],
     // creates an api call using url/kpi/kpis/
     // basically makes a function that gets
     // key performance indicators for the api call
@@ -11,9 +11,17 @@ export const api = createApi({
         getKpis: build.query<Array<GetKpisResponse>,void>({
             query: () => "kpi/kpis/",
             providesTags: ["Kpis"]
-        })
+        }),
+        getProducts: build.query<Array<GetProductsResponse>,void>({
+            query: () => "product/products",
+            providesTags: ["Products"]
+        }),
+        getTransactions: build.query<Array<GetTransactionsResponse>, void>({
+            query: () => "transaction/transactions/",
+            providesTags: ["Transactions"],
+        }),
     })
 })
 
 // makes a hook
-export const { useGetKpisQuery } = api;
+export const { useGetKpisQuery, useGetProductsQuery, useGetTransactionsQuery } = api;
--- client/src/state/types.ts
+++ client/src/state/types.ts
@@ -9,6 +9,27 @@ export interface GetKpisResponse{
     monthlyData: Array<Month>;
     dailyData: Array<Day>;
 }
+export interface GetProductsResponse{
+    id: string;
+    _id: string;
+    __v: number;
+    price: number;
+    expense: number;
+    transactions: Array<string>
+    createdAt: string;
+    updatedAt: string;
+}
+
+export interface GetTransactionsResponse {
+    id: string;
+    _id: string;
+    __v: number;
+    buyer: string;
+    amount: number;
+    productIds: Array<string>;
+    createdAt: string;
+    updatedAt: string;
+}
 
 export interface ExpensesByCategory{
     salaries: number;
