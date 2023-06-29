7ea14ea77d778ab1246ce6161a6c2489aa4450a3
abrahambeltran <abraham.beltran.2001@gmail.com>
2023-06-29 17:00:30 -0400

    completed Row 3

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,27 +1,29 @@
-import {createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { GetKpisResponse, GetProductsResponse, GetTransactionsResponse } from "./types";
-export const api = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
-    reducerPath: "main",
-    tagTypes: ["Kpis", "Products", "Transactions"],
-    // creates an api call using url/kpi/kpis/
-    // basically makes a function that gets
-    // key performance indicators for the api call
-    endpoints: (build) => ({
-        getKpis: build.query<Array<GetKpisResponse>,void>({
-            query: () => "kpi/kpis/",
-            providesTags: ["Kpis"]
-        }),
-        getProducts: build.query<Array<GetProductsResponse>,void>({
-            query: () => "product/products",
-            providesTags: ["Products"]
-        }),
-        getTransactions: build.query<Array<GetTransactionsResponse>, void>({
-            query: () => "transaction/transactions/",
-            providesTags: ["Transactions"],
-        }),
-    })
-})
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import {
+  GetKpisResponse,
+  GetProductsResponse,
+  GetTransactionsResponse,
+} from "./types";
 
-// makes a hook
-export const { useGetKpisQuery, useGetProductsQuery, useGetTransactionsQuery } = api;
+export const api = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
+  reducerPath: "main",
+  tagTypes: ["Kpis", "Products", "Transactions"],
+  endpoints: (build) => ({
+    getKpis: build.query<Array<GetKpisResponse>, void>({
+      query: () => "kpi/kpis/",
+      providesTags: ["Kpis"],
+    }),
+    getProducts: build.query<Array<GetProductsResponse>, void>({
+      query: () => "product/products/",
+      providesTags: ["Products"],
+    }),
+    getTransactions: build.query<Array<GetTransactionsResponse>, void>({
+      query: () => "transaction/transactions/",
+      providesTags: ["Transactions"],
+    }),
+  }),
+});
+
+export const { useGetKpisQuery, useGetProductsQuery, useGetTransactionsQuery } =
+  api;
